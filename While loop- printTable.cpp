ac6a98acd19cd54695b6a900b189f93c85a53793
Shikha Dixit <95029830+Shixit@users.noreply.github.com>
2023-04-23 22:48:55 +0530

    Create While loop- printTable.cpp

---
+++ While loop- printTable.cpp
@@ -0,0 +1,16 @@
+//User function Template for C++
+
+class Solution{
+    public:
+    void printTable(int n)
+    {
+        int m=10;
+        while(m!=0)
+        {
+           cout<<n*m<<" ";
+           m--;
+        }
+        
+        cout<<endl;
+    }  
+};
