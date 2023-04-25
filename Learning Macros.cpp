d442802850b4270a37d1b19b34c5f9ca1527c2b2
Shikha Dixit <95029830+Shixit@users.noreply.github.com>
2023-04-25 23:25:00 +0530

    Create Learning Macros.cpp

---
+++ Learning Macros.cpp
@@ -0,0 +1,11 @@
+//User function Template for C++
+
+void macros(int a,int b)
+{
+   // cin>>a>>b;
+    cout<<MULTI(a,b);
+    //See the macro defined at the top of the code. Use that macro to find a*b
+    //Use only macro to complete this task
+    
+    cout<<endl;
+}
