09c145d2155f9e7b180869c53571d588801ed054
Shikha Dixit <95029830+Shixit@users.noreply.github.com>
2023-04-27 15:29:39 +0530

    Create The New Line.cpp

---
+++ The New Line.cpp
@@ -0,0 +1,11 @@
+//User function Template for C++
+
+// Function to print each word in new line
+#include<iostream>
+using namespace std;
+void printNewLine()
+{
+ cout<<"Geeks"<<endl;    
+ cout<<"for"<<endl;   
+ cout<<"Geeks"<<endl; 
+}
